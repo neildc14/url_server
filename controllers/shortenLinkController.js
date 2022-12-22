8fd183b6575c98657494e866133332e117dba26b
neil dela cruz <genoznightmare@gmail.com>
2022-12-22 21:54:50 +0800

    fixed link leak to not registered users

--- controllers/shortenLinkController.js
+++ controllers/shortenLinkController.js
@@ -16,7 +16,7 @@ const getShortenLink = async (req, res) => {
     const shorten_link = await ShortenLink.findOne({ shorten_link: id }).exec();
     const has_user = shorten_link.user_id;
 
-    if (has_user === null && !user_id) {
+    if (has_user && !user_id) {
       return res.status(400).json({ error: "Request is not authorized" });
     }
     res.redirect(shorten_link.original_link);
